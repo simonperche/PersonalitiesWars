961e5cd867c10208bf432ce2812f7c4f6a3cb6ee
Solidras <solidras@gmail.com>
2021-12-14 11:44:14 +0100

    Adapt Image to slash command

--- src/images.py
+++ src/images.py
@@ -1,8 +1,9 @@
 import discord
 from discord.ext import commands
+from discord.commands import slash_command, Option
 
 from database import DatabasePersonality, DatabaseDeck
-
+import utils
 
 class Images(commands.Cog):
     def __init__(self, bot):
@@ -11,37 +12,42 @@ class Images(commands.Cog):
 
     #### Commands ####
 
-    @commands.command(description='Add a custom image to a personality."')
-    async def add_image(self, ctx, name, url):
+    @slash_command(description='Add a custom image to a personality."',
+                   guild_ids=utils.get_authorized_guild_ids())
+    async def add_image(self, ctx, name: Option(str, "Pick a name or write yours", autocomplete=utils.personalities_name_searcher),
+                        url: str):
         name = name.strip()
 
         id_perso = DatabasePersonality.get().get_perso_id(name)
 
         if not id_perso:
-            await ctx.message.add_reaction(u"\u274C")
-            await ctx.send(f'Personality **{name}** not found.')
-            return
-
-        if not url:
-            await ctx.message.add_reaction(u"\u274C")
-            await ctx.send(f'Please give an URL to the image.')
+            await ctx.respond(f'Personality **{name}** not found.')
+            msg = await ctx.interaction.original_message()
+            await msg.add_reaction(u"\u274C")
             return
 
         DatabasePersonality.get().add_image(id_perso, url)
         # Green mark
-        await ctx.message.add_reaction(u"\u2705")
+        await ctx.respond('Done.')
+        msg = await ctx.interaction.original_message()
+        await msg.add_reaction(u"\u2705")
 
-    @commands.command(description='Remove an image of a personality."')
-    async def remove_image(self, ctx, name, url):
+    @slash_command(description='Remove an image of a personality."',
+                   guild_ids=utils.get_authorized_guild_ids())
+    async def remove_image(self, ctx, name: Option(str, "Pick a name or write yours", autocomplete=utils.personalities_name_searcher),
+                           url: str):
         name = name.strip()
 
         id_perso = DatabasePersonality.get().get_perso_id(name)
 
         if not id_perso:
-            await ctx.message.add_reaction(u"\u274C")
-            await ctx.send(f'Personality **{name}** not found.')
+            await ctx.respond(f'Personality **{name}** not found.')
+            msg = await ctx.interaction.original_message()
+            await msg.add_reaction(u"\u274C")
             return
 
         DatabasePersonality.get().remove_image(id_perso, url)
         # Green mark
-        await ctx.message.add_reaction(u"\u2705")
+        await ctx.respond('Done.')
+        msg = await ctx.interaction.original_message()
+        await msg.add_reaction(u"\u2705")
